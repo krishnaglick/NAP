cb90786c7357e4b376dede8bf22696f9dd78e303
Krishna Glick <unstoppable411@gmail.com>
2017-08-10 13:17:05 -0400

    first

---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+  public GameObject MenuPanel;
+
+  // Use this for initialization
+  void Start()
+  {
+    MenuPanel.SetActive(true);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void StartGame()
+  {
+    Debug.Log("Starting Game");
+    SceneManager.LoadScene("StageOne");
+
+  }
+
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+}
---
+++ Assets/MarioControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioControl : MonoBehaviour {
+
+  public GameObject mario;
+
+  public Sprite standRight;
+  public Sprite standLeft;
+
+  private bool facingRight = true;
+
+	void Start () {
+  }
+	
+	// Update is called once per frame
+	void Update() {
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      mario.GetComponent<Animator>().Play("walk_right");
+      mario.GetComponent<Animator>().enabled = true;
+      facingRight = true;
+    }
+    else if(Input.GetKey(KeyCode.LeftArrow)) {
+      mario.GetComponent<Animator>().Play("walk_left");
+      mario.GetComponent<Animator>().enabled = true;
+      facingRight = false;
+    }
+    else if (Input.GetKey(KeyCode.UpArrow)) {
+      mario.GetComponent<Animator>().enabled = true;
+      if(facingRight)
+        mario.GetComponent<Animator>().Play("jump_right");
+      else
+        mario.GetComponent<Animator>().Play("jump_left");
+    }
+    else {
+      mario.GetComponent<Animator>().enabled = false;
+      if (facingRight)
+        mario.GetComponent<SpriteRenderer>().sprite = standRight;
+      else
+        mario.GetComponent<SpriteRenderer>().sprite = standLeft;
+    }
+  }
+}
