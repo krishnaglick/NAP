b0a8f01345ffe0d47ca987ffb35315e0b6a097df
Krishna Glick <unstoppable411@gmail.com>
2017-08-25 20:11:17 -0400

    whoops

--- Assets/Tactics RPG/Assets/Scripts/Enums/Genders.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum Genders
-{
-	Male,
-	Female,
-	Unknown
-}
--- Assets/Tactics RPG/Assets/Scripts/Model/LevelData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class LevelData : ScriptableObject 
-{
-	public List<Vector3> tiles;
-}
